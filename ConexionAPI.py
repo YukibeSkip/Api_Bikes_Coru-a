ab3b92b421a3bf6e16205ad18bed52028a2c0ff6
root <root@DESKTOP-I3KBI7V.>
2024-12-16 13:07:59 +0100

    nuevo scrip

--- ConexionAPI.py
+++ ConexionAPI.py
@@ -1,7 +1,8 @@
 
 
 import requests
-from pymongo import MongoClient
+import json
+#from pymongo import MongoClient
 
 # try:
 #     eri = "mongodb://localhost:27017/"
@@ -13,11 +14,39 @@ from pymongo import MongoClient
 # except Exception as e:
 #     raise Exception(
 #         "The following error occurred: ", e)
+
 endpoint = "http://api.citybik.es/v2/networks"
 
-datos = requests.get(endpoint)
+id = []
+name =[]
+location = []
+href = []
+company = []
+license =[]
+ebikes =[]
+gbfs_href =[]
+stations =[]
 
-# for network in datos['network']:
-#         # Verificamos si el nombre de la red es 'BiciCoruña'
-#         if network['name'] == 'Bicicoruña':
-#             print(network)
+
+import requests
+
+
+response = requests.get(endpoint)
+
+
+# Verificar si la solicitud fue exitosa
+print(response)
+
+# print(response)
+
+# for i in paradas["network"]:
+#     name.append(i["name"])
+#     stations.append(i['stations'])
+#     company.append(i['company'])
+#     href.append(i['href'])
+#     location.append(i['location'])
+#     id.append(i["id"])
+
+# datos = {"Name":name, "Sations":stations, "Company":company, "href":href, "Location":location, "Id":id}
+
+#print(datos)
